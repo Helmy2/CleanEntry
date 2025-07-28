bd6188da433dee0ce168873f11e5ab9c00309957
Helmy2 <mohelmy.eng@gmail.com>
2025-07-28 10:12:55 +0300

    Add shimmer effect for loading states in CountryCodePicker

--- feature_auth/src/main/java/com/example/clean/entry/feature_auth/presentation/components/CountryRow.kt
+++ feature_auth/src/main/java/com/example/clean/entry/feature_auth/presentation/components/CountryRow.kt
@@ -1,12 +1,15 @@
 package com.example.clean.entry.feature_auth.presentation.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.HorizontalDivider
@@ -16,7 +19,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.clean.core.components.shimmer
 import com.example.clean.entry.core.design_system.spacing
 import com.example.clean.entry.feature_auth.domain.model.Country
 import com.example.clean.feature_auth.R
@@ -29,7 +35,6 @@ fun CountryRow(
     modifier: Modifier = Modifier
 ) {
     Column {
-
         Row(
             modifier = modifier
                 .fillMaxWidth()
@@ -61,6 +66,46 @@ fun CountryRow(
                     tint = MaterialTheme.colorScheme.primary
                 )
             }
+        }
+        HorizontalDivider()
+    }
+}
+
+@Composable
+fun CountryRowShimmer(modifier: Modifier = Modifier) {
+    Column {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(MaterialTheme.spacing.small),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(32.dp)
+                    .height(MaterialTheme.typography.titleLarge.lineHeight.value.dp)
+                    .clip(RoundedCornerShape(10))
+                    .shimmer()
+            )
+            Spacer(modifier = Modifier.width(MaterialTheme.spacing.medium))
+
+            Box(
+                modifier = Modifier
+                    .width(32.dp)
+                    .height(MaterialTheme.typography.bodyLarge.lineHeight.value.dp)
+                    .clip(RoundedCornerShape(10))
+                    .shimmer()
+            )
+
+            Spacer(modifier = Modifier.width(MaterialTheme.spacing.small))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(MaterialTheme.typography.bodyLarge.lineHeight.value.dp)
+                    .clip(RoundedCornerShape(10))
+                    .shimmer()
+            )
         }
         HorizontalDivider()
     }
