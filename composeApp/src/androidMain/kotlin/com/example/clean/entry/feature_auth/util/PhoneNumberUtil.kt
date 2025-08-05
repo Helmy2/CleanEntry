5bae471836230e484570e912b0f2e4e90d724455
Helmy2 <mohelmy.eng@gmail.com>
2025-08-05 15:57:58 +0300

    Refactor: Restructure authentication module and implement platform-specific dependencies

---
+++ composeApp/src/androidMain/kotlin/com/example/clean/entry/feature_auth/util/PhoneNumberUtil.kt
@@ -0,0 +1,13 @@
+package com.example.clean.entry.feature_auth.util
+
+import io.michaelrocks.libphonenumber.android.PhoneNumberUtil
+import io.michaelrocks.libphonenumber.android.Phonenumber
+
+actual class PhoneNumberUtil(
+    private val phoneNumberUtil: PhoneNumberUtil
+) {
+    actual fun isValidNumber(phone: String, regionCode: String): Boolean {
+        val phoneNumberProto: Phonenumber.PhoneNumber = phoneNumberUtil.parse(phone, regionCode)
+        return phoneNumberUtil.isValidNumber(phoneNumberProto)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/example/clean/entry/feature_auth/di/authModule.ios.kt
@@ -0,0 +1,6 @@
+package com.example.clean.entry.feature_auth.di
+
+import org.koin.core.module.Module
+
+actual val authPlatformModule: Module
+    get() = TODO("Not yet implemented")
