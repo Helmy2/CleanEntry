8e8e974692fa43e15946002d9118d7c4709d1c2f
Helmy2 <mohelmy.eng@gmail.com>
2025-08-08 17:31:46 +0300

    Refactor: Update LoginViewController and RegistrationViewController to use CleanEntryTheme, and adjust Room library dependencies

--- composeApp/src/iosMain/kotlin/com/example/clean/entry/feature_auth/presentation/login/LoginViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/clean/entry/feature_auth/presentation/login/LoginViewController.kt
@@ -3,6 +3,7 @@ package com.example.clean.entry.feature_auth.presentation.login
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.window.ComposeUIViewController
+import com.example.clean.entry.core.design_system.CleanEntryTheme
 import com.example.clean.entry.feature_auth.domain.model.Country
 import com.example.clean.entry.feature_auth.util.NativeViewFactory
 import platform.UIKit.UIViewController
@@ -21,14 +22,17 @@ fun LoginViewController(
     clearCountryResult: () -> Unit,
 ): UIViewController {
     return ComposeUIViewController {
-        CompositionLocalProvider(LocalNativeViewFactory provides nativeViewFactory) {
-        LoginRoute(
-            viewModel = viewModel,
-            onNavigateToCountryPicker = onNavigateToCountryPicker,
-            onLoginSuccess = onLoginSuccess,
-            onCreateAccountClick = onCreateAccountClick,
-            countryResult = countryResult,
-            clearCountryResult = clearCountryResult
-        )}
+        CleanEntryTheme {
+            CompositionLocalProvider(LocalNativeViewFactory provides nativeViewFactory) {
+                LoginRoute(
+                    viewModel = viewModel,
+                    onNavigateToCountryPicker = onNavigateToCountryPicker,
+                    onLoginSuccess = onLoginSuccess,
+                    onCreateAccountClick = onCreateAccountClick,
+                    countryResult = countryResult,
+                    clearCountryResult = clearCountryResult
+                )
+            }
+        }
     }
 }
