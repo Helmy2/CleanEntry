b6cfefe1b99ec80376e9ac3502750caad012b968
helmy <mohelmy.eng@gmail.com>
2025-09-25 20:35:34 +0300

    add platform-specific secrets utility implementations for jvm and wasm/js

---
+++ core/src/wasmJsMain/kotlin/com/example/clean/entry/core/util/Secrets.wasmJs.kt
@@ -0,0 +1,11 @@
+package com.example.clean.entry.core.util
+
+actual object Secrets {
+    actual fun getPexelsApiKey(): String {
+        return "ANAcf7Uluv3bBl4FzKZ1uTCZgY4KtsTPBe8TLFPrfvhTXF06C4GsrC43"
+    }
+
+    actual fun getFirebaseApiKey(): String {
+        return "AIzaSyCIoWUnayhp80yTh6QHUgj2EPHhUFybOiM"
+    }
+}
